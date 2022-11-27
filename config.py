58ecfd0f6cffc6fc57288e259b96781e1d075774
Jaysuuun <72870556+Jaysuuun@users.noreply.github.com>
2022-11-27 21:13:41 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from os import getenv
-
-DB_HOST=getenv("DB_HOST")
-DB_PORT=getenv("DB_PORT")
-DB_NAME=getenv("DB_NAME")
-DB_USERNAME=getenv("DB_USERNAME")
-DB_PASSWORD= getenv("DB_PASSWORD")
-SECRET_KEY = getenv("SECRET_KEY")
-#SQLALCHEMY_DATABASE_URI = getenv("SQLALCHEMY_DATABASE_URI")
